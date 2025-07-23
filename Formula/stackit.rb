2391f37e6d37200f32dd7ab83c4be34807bcba70
Marcel Jacek <Marcel.Jacek@stackit.cloud>
2025-07-23 10:25:34 +0200

    update deprecation message

--- Formula/stackit.rb
+++ Formula/stackit.rb
@@ -6,9 +6,9 @@ class Stackit < Formula
   desc "Demo: A command-line interface to manage STACKIT resources.
 This CLI is in a beta state. More services and functionality will be supported soon."
   homepage "https://github.com/stackitcloud/stackit-cli"
-  version "0.37.0"
+  version "0.37.1"
   license "Apache-2.0"
-  disable! date: "2026-01-22", because: "the cask should be used now instead", replacement_cask: "stackit"
+  disable! date: "2026-01-22", because: "the cask should be used now instead. The formula will be no longer updated", replacement_cask: "stackit"
 
 
   on_macos do
